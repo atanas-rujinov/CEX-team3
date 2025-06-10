0979968e2a5060e0a32006fd94d14eed879927cc
Явор Минковски <101576942+Javorcho@users.noreply.github.com>
2025-06-10 14:25:48 +0300

    Delete cex_yavor/springboot/crypto_exchange directory

--- cex_yavor/springboot/crypto_exchange/src/main/java/com/crypto_exchange/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.crypto_exchange.service;
-
-public interface UserService {
-    boolean deleteAccount(String username, String password);
-    boolean editUsername(String currentUsername, String newUsername);
-    boolean banAccount(String username);
-} 
